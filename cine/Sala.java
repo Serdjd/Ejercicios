f9110196ee51bb19932f85c8c7b935592654c9c7
Serdjd <sergiodobladomunoz@gmail.com>
2023-03-23 19:27:55 +0100

    Esto no funca

--- cine/Sala.java
+++ cine/Sala.java
@@ -1,15 +1,19 @@
 package cine;
-import java.util.Map;
-import java.util.HashMap;
-
+import java.util.ArrayList;
 
 public class Sala {
-    private StringBuilder[][] sala;
+    private ArrayList<StringBuilder [][]>  sala;
     
-    public StringBuilder[][] getSala() {
-        return sala;
+    public StringBuilder[][] getSala(int num) {
+        if(num==0){
+            return sala.get(0);
+        }
+        else{
+            return sala.get(1);
+        }
+        
     }
-    public void setSala(StringBuilder[][] sala) {
+    public void setSala(ArrayList<StringBuilder [][]>  sala) {
         this.sala = sala;
     }
     
@@ -20,22 +24,23 @@ public class Sala {
     public static final String Red="\u001B[31m";
     public static final String Green = "\u001B[32m";
     public static final String Reset = "\u001B[0m";
-    public static StringBuilder[][] Generar_sala(Pelicula_ film){
-        StringBuilder hall[][];
-        Map<StringBuilder ,Espectador> publi = new HashMap<>();
-        hall = new StringBuilder[8][7];
+
+    public static ArrayList<ArrayList> Generar_sala(Pelicula_ film){
+        ArrayList<StringBuilder [][]> salas = new ArrayList<>();
+        StringBuilder hall[][] = new StringBuilder[10][9];
+        ArrayList <Espectador> pers = new ArrayList<>();
         String letras = "ABCDEFGHI";
-        int personas = 0;
-        for(int i=0;i<8;i++){
-            for(int j=8;j>1;j--){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<8;j++){
+                hall[i][j] = new StringBuilder();
+                pers[i][j] = new StringBuilder();
                 String color = Generadores.rand_asientos();
-                hall[i][j-1].append(color+j+letras.charAt(i)+Reset);
+                hall[i][j].append(color+(j+1)+letras.charAt(i)+Reset);
                 if(color.equals(Red)){
                         Espectador publico = new Espectador();
-                        personas++;
                     if(publico.getDinero()>=film.getPrecio()){
                         if(publico.getEdad()>=film.getEdad_minima()){
-                            publi.put(hall[i][j-1],publico);
+                            pers[i][j].append(publico.getNombre()+(" ")+publico.getApellido());
                         }
                         else{
                             System.out.println(publico.getNombre()+publico.getApellido()+" no tiene la edad suficiente");
@@ -47,7 +52,14 @@ public class Sala {
                 }
             }
         }
-        System.out.println(personas*film.getPrecio());
-        return hall;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<8;j++){
+                System.out.print(hall[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+        salas.add(hall);
+        salas.add(pers);
+        return salas;
     }
 }
