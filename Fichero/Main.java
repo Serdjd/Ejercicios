c8fb25f33b24ac9ea7df27754c023b7413d0701d
Serdjd <segiodobladomunoz@gmail.com>
2023-04-21 09:15:02 +0200

    ficheros empezados

---
+++ Fichero/Main.java
@@ -0,0 +1,26 @@
+package Fichero;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String palabra_salida=("exit");
+        String palabra_entrada=("");
+        FileReader in = new FileReader("/home/serdjd_05/Pa ficheros.txt");
+        //Con el filereader podemos leer lo que hay en el archivo
+        FileWriter out = new FileWriter("/home/serdjd_05/Pa ficheros.txt",true);
+        //Con el filewriter escribimos dentro del archivo y si añadimo true lo anterior no lo borramos
+        Scanner leer = new Scanner(System.in);
+        while(palabra_entrada.compareTo(palabra_salida)!=0){
+            System.out.println("Dime lo que quieras añadir al fichero");
+            palabra_entrada = leer.nextLine();
+            if(palabra_entrada.compareTo(palabra_salida)!=0){
+                out.write("\n"+palabra_entrada);
+            }
+        }
+            out.close();
+    }
+    
+}
